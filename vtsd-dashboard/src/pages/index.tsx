f4704cab400de27b35e758ad497edc3c228def2a
vogelcodes <54858106+vogelcodes@users.noreply.github.com>
2023-10-28 18:48:27 +0000

    Hotmart-API

--- vtsd-dashboard/src/pages/index.tsx
+++ vtsd-dashboard/src/pages/index.tsx
@@ -1,7 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import Head from "next/head";
 import Link from "next/link";
 
-export default function Home() {
+export default function Home({ response, body }: {response: string, body:{"access_token": string, "token_type": string, "expires_in": number, scope: string, jti: string} }) {
   return (
     <>
       <Head>
@@ -12,7 +13,7 @@ export default function Home() {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+            Hotmart status: {response}
           </h1>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
@@ -42,4 +43,19 @@ export default function Home() {
       </main>
     </>
   );
+}
+export async function getServerSideProps () {
+  const response = await fetch(`https://api-sec-vlc.hotmart.com/security/oauth/token?grant_type=client_credentials&client_id=${process.env.HOTMART_API_CLIENT_ID}&client_secret=${process.env.HOTMART_CLIENT_SECRET}`, {method: "POST", 'headers': {
+    'Content-Type': 'application/json',
+    'Authorization': `Basic ${process.env.HOTMART_BASIC_HASH}`
+  },})
+
+  const body: {"access_token": string, "token_type": string, "expires_in": number, scope: string, jti: string} = await response.json()
+  console.log(body)
+
+  return {props: {
+            response: response.status,
+            body: body
+                }
+        }
 }
